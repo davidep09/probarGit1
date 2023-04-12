c91b68aa21273d35f83ac5095c9fa7202ecefa9b
davidep <davidep@alumnos.iesgalileo.es>
2023-04-12 19:49:54 +0200

    v1.1: * Alumno.java métodos * Programa.java probar métodos

--- Alumno.java
+++ Alumno.java
@@ -1,11 +1,25 @@
 class Alumno {
+
    // Atributos
    private int nm;
    private String nombre;
    
    // Constructor
-   public Alumno(int nm, String nombre){
+   public Alumno(int nm, String nombre) {
       this.nm = nm;
       this.nombre = nombre;
+   }
+   
+   // Métodos
+   public int getNm() {
+      return nm;
+   }
+   
+   public String getNombre() {
+      return nombre;
+   }
+   
+   public String toString() {
+      return "Alumno[nm = " + nm + ", nombre = " + nombre + "]";
    }
 }
--- Programa.java
+++ Programa.java
@@ -1,5 +1,8 @@
 class Programa {
    public static void main(String[] args){
-      
+      Alumno a1 = new Alumno(1, "Pepe");
+      System.out.println("Matrícula = " + a1.getNm());
+      System.out.println("Nombre = " + a1.getNombre());
+      System.out.println(a1);
    }
 }
