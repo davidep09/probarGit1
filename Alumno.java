b14cc3435f9f2af5dcc53a3c8775768d85c88390
davidep <davidep@alumnos.iesgalileo.es>
2023-04-12 19:36:49 +0200

    v1: * Alumno.java contructor * Programa.java vacío

---
+++ Alumno.java
@@ -0,0 +1,11 @@
+class Alumno {
+   // Atributos
+   private int nm;
+   private String nombre;
+   
+   // Constructor
+   public Alumno(int nm, String nombre){
+      this.nm = nm;
+      this.nombre = nombre;
+   }
+}
